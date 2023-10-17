ca3bfeaad333d03782f468176da6a058bb97807a
Ashif vk <ashifvk503@gmail.com>
2023-10-17 19:53:55 +0530

    ok

--- backend/settings.py
+++ backend/settings.py
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -134,6 +134,8 @@ STATIC_URL = 'static/'
 MEDIA_URL='./media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -152,6 +154,4 @@ ALLOWED_HOSTS=[]
 
 
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
 
